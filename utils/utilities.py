c071e491178456e8f2064b19bb2ba8ce5f4188ff
kongqiuqiang <kongqiuqiang@bytedance.com>
2020-06-08 13:00:44 +0800

    add mixup

--- utils/utilities.py
+++ utils/utilities.py
@@ -113,13 +113,36 @@ def d_prime(auc):
     return d_prime
 
 
+class Mixup(object):
+    def __init__(self, mixup_alpha, random_seed=1234):
+        """Mixup coefficient generator.
+        """
+        self.mixup_alpha = mixup_alpha
+        self.random_state = np.random.RandomState(random_seed)
+
+    def get_lambda(self, batch_size):
+        """Get mixup random coefficients.
+        Args:
+          batch_size: int
+        Returns:
+          mixup_lambdas: (batch_size,)
+        """
+        mixup_lambdas = []
+        for n in range(0, batch_size, 2):
+            lam = self.random_state.beta(self.mixup_alpha, self.mixup_alpha, 1)[0]
+            mixup_lambdas.append(lam)
+            mixup_lambdas.append(1. - lam)
+
+        return np.array(mixup_lambdas)
+
+
 class StatisticsContainer(object):
     def __init__(self, statistics_path):
         """Contain statistics of different training iterations.
         """
         self.statistics_path = statistics_path
 
-        self.backup_statistics_path = '{}_{}.pickle'.format(
+        self.backup_statistics_path = '{}_{}.pkl'.format(
             os.path.splitext(self.statistics_path)[0], 
             datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
 
